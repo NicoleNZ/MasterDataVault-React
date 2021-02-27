41f88ee40e618d89ca97ece564f0bd3cf86a3129
Nicole <nicole.barnett.nz@gmail.com>
2021-02-27 17:07:12 +1300

    Adding functionality to ProductList component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { CreateProduct } from "./CreateProduct";
+import { EditProduct } from "./EditProduct";
+import { DeleteProduct } from "./DeleteProduct";
+import { ProductList } from "./ProductList";
 
 const Home = () => {
     
@@ -45,11 +49,50 @@ const Home = () => {
             };
         const newProducts = [...productList];
         newProducts.push(newProduct); //this is pushing the newly created product into the array of product objects
-
-        setProductList(newProducts); //this 'updates' the productList to contain the newly created product
+        setProductList(newProducts); //this 'updates' the productList in the memory to contain the newly created product
         
-        }
-}
+        fetch("mongodb://localhost:27017/masterDataVault-React", { //this is going to POST the newly created product to the MongoDB database
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newProduct),
+        })
+        .then((response) => {
+        console.log("MongoDB response: ", response);
+        });   
+    };
+
+
+    //next we are getting the latest data from MongoDB and updating the product list that is rendered on the UI
+    useEffect(() => {
+    fetch("mongodb://localhost:27017/masterDataVault-React", { //this is going to GET the list of products from the MongoDB database, including the new one just added
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+    }
+    })
+    .then((response) => {
+        console.log("MongoDB product list response: ", response);
+        return response.json();
+    })
+    .then((productData) => {
+        console.log("Current productData is: ", productData);
+        setProductList(productData.data); //is this incase data is added to the databse from elsewhere?  Otherwise, how is this not a repeat of setProductList(newProducts);
+    });
+    }, []);  
+    
+    return (
+        <div>
+        <ProductList products={productList} />
+        <CreateProduct submit={handleCreateProductSubmit}/>
+        <EditProduct />
+        <DeleteProduct />
+        </div>
+    );
+
+};
+
 
 
 export { Home };
---
+++ client/src/components/ProductList.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ProductList = (props) => {
+    return (
+        <ul>
+            {props.products.map((el, index) => ( //this props.product is related to the products={productList} in the render of Home component
+                <li key={index}>
+                    Product Code: {el.productCode}
+                    Product Name: {el.productName}
+                    Net Weight: {el.netWeight}
+                    Units per Case: {el.unitsPerCase}
+                    List Price: {el.listPrice}
+                    Unit Barcode: {el.unitBarcode}
+                    Case Barcode: {el.caseBarcode}
+                    Active Product: {el.activeProduct}
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export { ProductList }
